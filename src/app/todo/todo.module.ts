ee95a5368670057b23f5b369cde56f8924c05581
andresacelas <andresacelas@outlook.com>
2021-09-13 15:48:48 -0500

    home component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
+import { HomeComponent } from './containers/home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     TodoRoutingModule
