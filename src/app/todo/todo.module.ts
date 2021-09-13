ac5571db13f5222654f8cb5789464e2152632994
andresacelas <andresacelas@outlook.com>
2021-09-13 16:24:31 -0500

    input component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { TodoRoutingModule } from './todo-routing.module';
 import { HomeComponent } from './containers/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { InputComponent } from './components/input/input.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    InputComponent
   ],
   imports: [
     CommonModule,
