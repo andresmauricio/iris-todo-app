281449452be4908674649b6ff545a17e09e4fcf8
andresacelas <andresacelas@outlook.com>
2021-09-13 16:58:00 -0500

    preview task in component home

--- src/app/todo/containers/home/home.component.ts
+++ src/app/todo/containers/home/home.component.ts
@@ -3,18 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public tasks = [
+    { status: true, name: 'Desarrollar componente tabla Vue.js', id: 1 },
+    { status: false, name: 'Relizar ibntegración Paypal', id: 2 },
+    { status: true, name: 'Levantar requisitos nuevos', id: 3 },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addTask(task: string) {
     console.log(task);
-    
   }
-
 }
